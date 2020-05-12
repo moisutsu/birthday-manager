55948bf1fac2b505d6177cfdf65407fa08a78fd5
moisutsu <moisutsu@gmail.com>
2020-05-13 00:34:05 +0900

    Headerを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,22 @@ import React from "react";
 import styled from 'styled-components';
 import Link from "next/link";
 
+import Header from "src/components/Header";
+
 import { baseStyle } from "src/styles";
 
 const Component: React.FCX = () => {
   return (
-    <main>
-      <h1>
-        Index Page!
-      </h1>
-      <Link href="/add"><a>Add</a></Link>
-      <Link href="/remove"><a>Remove</a></Link>
-    </main>
+    <>
+      <Header />
+        <main>
+          <h1>
+            Index Page!
+          </h1>
+          <Link href="/add"><a>Add</a></Link>
+          <Link href="/remove"><a>Remove</a></Link>
+        </main>
+    </>
   )
 }
 
