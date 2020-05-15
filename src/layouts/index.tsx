388ef338b7f8ce583591ccdfb2a0b01a678cc5e0
moisutsu <moisutsu@gmail.com>
2020-05-15 14:31:16 +0900

    metaタグの変更

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,7 +8,7 @@ const Layout: React.FunctionComponent = ({ children }) => (
     <Head>
       <title>Birthday-Manager</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0, viewport-fit=cover, shrink-to-fit=no' />
     </Head>
     <Header />
     {children}
--- src/styles/base-style.ts
+++ src/styles/base-style.ts
@@ -6,10 +6,7 @@ export default css`
   max-width: 1400px;
   padding: 10vh 2vw 5vw 2vw;
   margin: 0 auto;
-  > h1 {
-    padding-bottom: 3rem;
-    font-size: 4rem;
-  }
+  
   @media screen and (max-width: 1100px) {
   }
   @media screen and (max-width: 768px) {
